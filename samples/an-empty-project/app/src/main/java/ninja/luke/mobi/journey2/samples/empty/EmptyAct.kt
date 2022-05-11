3fd779507b44e66cf2f3105aca6b486de2fe42b0
Nguyen IT. Le <nguyenl@techcombank.com.vn>
2022-05-11 14:40:19 +0700

    add empty project

---
+++ samples/an-empty-project/app/src/main/java/ninja/luke/mobi/journey2/samples/empty/EmptyAct.kt
@@ -0,0 +1,6 @@
+package ninja.luke.mobi.journey2.samples.empty
+
+import androidx.appcompat.app.AppCompatActivity
+
+class EmptyAct : AppCompatActivity() {
+}
---
+++ samples/an-empty-project/app/src/main/java/ninja/luke/mobi/journey2/samples/empty/EmptyApp.kt
@@ -0,0 +1,6 @@
+package ninja.luke.mobi.journey2.samples.empty
+
+import android.app.Application
+
+class EmptyApp: Application() {
+}
